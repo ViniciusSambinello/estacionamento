368a1d89a1550880dabce6a8e96cb60fc5ad1882
Adriano Ramazzotte <33229587+adrianoramazzotte@users.noreply.github.com>
2025-09-18 20:20:09 -0300

    desenvolvimento

--- src/main/java/com/example/demo/domain/FormaPagamento.java
+++ src/main/java/com/example/demo/domain/FormaPagamento.java
@@ -1,5 +1,25 @@
 package com.example.demo.domain;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "formapagamento")  // nome da tabela no banco
+@Data                     // gera getters, setters, toString, equals e hashCode
+@NoArgsConstructor         // gera construtor sem argumentos
+@AllArgsConstructor  
 public class FormaPagamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
 
 }
